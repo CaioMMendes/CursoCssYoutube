a2e330559c909263f579a78dbb88bd7ab6503c28
Caio Martins <115363966+CaioMMendes@users.noreply.github.com>
2022-12-04 23:06:26 -0300

    Add files via upload

---
+++ projeto.js
@@ -0,0 +1,29 @@
+var lista = []
+var numeroDeImagens = 8
+var ii = 1
+var i = numeroDeImagens - 1
+var gs = [5800, 0, 500, 4800, 5800, 0, 6500, 0]
+
+
+for (numeroDeImagens; numeroDeImagens >= 1; numeroDeImagens--) {
+    lista.push(`<div class='card'>
+                <div class='product-image' id='img-${ii}'></div>
+                <h3>Char ${ii}</h3>
+                <p>Este char é muito forte...</p>
+                <p>GS <span class='bold'>${gs[(ii-1)]}</span>
+                </p>
+                <a href='#' class='btn'>Vizualizar</a>
+            </div>`)
+    ii++
+}
+
+for (i; i >= 0; i--) {
+    mostrar()
+}
+
+
+
+
+function mostrar() {
+    $('.chars-container').after(lista[i])
+}
